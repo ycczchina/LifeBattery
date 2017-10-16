9726b2314802d4990a047e2971e552393460a532
ycczchina <ycchenzheng@gmail.com>
2017-10-16 15:49:06 -0400

    Apply date data

--- app/src/main/java/com/example/zheng/lifebattery/MainActivity.java
+++ app/src/main/java/com/example/zheng/lifebattery/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.Toast;
 
@@ -14,27 +13,19 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-    private RoundedRectProgressBar bar;
     private BatteryView bv;
-    private Button btn;
     private FloatingActionButton fba1;
     private int progress;
     private Timer timer;
-    private Day d;
+    private int year_birth, month_birth, day_birth;
+    private int year_cur, month_cur, day_cur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bar = (RoundedRectProgressBar) findViewById(R.id.bar);
         bv = (BatteryView) findViewById(R.id.bv);
-        btn = (Button) findViewById(R.id.btn);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                    reset();
-            }
-        });
+
         fba1 = (FloatingActionButton) findViewById(R.id.fab1);
         fba1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,25 +35,18 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
-
-
-
-
     }
 
-    /**
-     * 进度条从头到尾跑一次
-     */
-    private void reset() {
+    //Charging Animation
+    private void reset(final int percent) {
         progress = 0;
         timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                bar.setProgress(progress);
                 bv.setProgress(progress);
                 progress ++;
-                if (progress > 100) {
+                if (progress > percent) {
                     timer.cancel();
                 }
             }
@@ -70,27 +54,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void showDialogPick(final FloatingActionButton fba) {
+    private void showDialogPick(final FloatingActionButton fba1) {
         final StringBuffer time = new StringBuffer();
-        //获取Calendar对象，用于获取当前时间
         final Calendar calendar = Calendar.getInstance();
         int year = calendar.get(Calendar.YEAR);
-        int month = calendar.get(Calendar.MONTH);
+        final int month = calendar.get(Calendar.MONTH);
         int day = calendar.get(Calendar.DAY_OF_MONTH);
-        //实例化DatePickerDialog对象
+        year_cur = year;
+        month_cur = month + 1;
+        day_cur = day;
+
         DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
-            //选择完日期后会调用该回调函数
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                //因为monthOfYear会比实际月份少一月所以这边要加1
                 time.append(year + "-" + (monthOfYear+1) + "-" + dayOfMonth);
-                //选择完日期后弹出选择时间对话框
+                year_birth = year;
+                month_birth = monthOfYear + 1;
+                day_birth = dayOfMonth;
                 Toast.makeText(MainActivity.this,"Your birthday is "+time,Toast.LENGTH_SHORT).show();
+
+                int percent = 100 - ((((day_cur - day_birth) / 30) + month_cur - month_birth) / 12 + year_cur - year_birth) * 100 / 70;
+
+                reset(percent);
             }
         }, year, month, day);
-        //弹出选择日期对话框
+
         datePickerDialog.show();
+
     }
+
+
 
 
 }
